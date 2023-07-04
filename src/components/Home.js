9b57ea73acedbcc749b77e7cca74b6ebdba115dd
Ankit Swami <Swamiankit139@gmail.com>
2023-07-04 06:54:18 +0530

    NoteItem + Note Completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,20 +8,20 @@ const Home = () => {
       <form className='mt-3 container'>
         <div className="mb-3">
           <label htmlFor="exampleInput" className="form-label">Title</label>
-          <input type="text" className="form-control" id="exampleInput" aria-describedby="emailHelp"/>
+          <input type="text" className="form-control" id="exampleInput" aria-describedby="emailHelp" />
         </div>
         <div className="mb-3">
           <label htmlFor="exampleInput" className="form-label">Description</label>
-          <input type="text" className="form-control" id="exampleInput"/>
+          <input type="text" className="form-control" id="exampleInput" />
         </div>
         <div className="mb-3">
           <label htmlFor="exampleInput" className="form-label">Category</label>
-          <input type="text" className="form-control" id="exampleInput"/>
+          <input type="text" className="form-control" id="exampleInput" />
         </div>
-        <button type="submit" className="btn btn-primary mt-2" style={{"width" : "100%"}}>Submit</button>
+        <button type="submit" className="btn btn-primary mt-2" style={{ "width": "100%" }}>Submit</button>
       </form>
-      <hr/><hr/>
-     <Notes/>
+      <hr /><hr />
+      <Notes />
     </div>
   )
 }
--- src/context/Notes/NoteState.js
+++ src/context/Notes/NoteState.js
@@ -1,107 +1,107 @@
 import { useState } from "react";
 import NoteContext from "./noteContext";
 
-const NoteState = (props) =>{
-    const notesInitial = [
-        {
-          "_id": "64a2b86414bc162789af30315",
-          "user": "64972653d9a8bee801567330",
-          "title": "Movie",
-          "description": "Complete movie OMG 2",
-          "tag": "entertainment",
-          "date": "2023-07-03T12:00:36.350Z",
-          "__v": 0
-        },
-        {
-          "_id": "64a2b899014bc162789af3317",
-          "user": "64972653d9a8bee801567330",
-          "title": "React",
-          "description": "Complete React Course",
-          "tag": "study",
-          "date": "2023-07-03T12:01:20.922Z",
-          "__v": 0
-        },
-        {
-          "_id": "64a2b86414bc1627889af3315",
-          "user": "64972653d9a8bee801567330",
-          "title": "Movie",
-          "description": "Complete movie OMG 2",
-          "tag": "entertainment",
-          "date": "2023-07-03T12:00:36.350Z",
-          "__v": 0
-        },
-        {
-          "_id": "64a2b89014bc1627789af3317",
-          "user": "64972653d9a8bee801567330",
-          "title": "React",
-          "description": "Complete React Course",
-          "tag": "study",
-          "date": "2023-07-03T12:01:20.922Z",
-          "__v": 0
-        },
-        {
-          "_id": "64a2b866414bc162789af3315",
-          "user": "64972653d9a8bee801567330",
-          "title": "Movie",
-          "description": "Complete movie OMG 2",
-          "tag": "entertainment",
-          "date": "2023-07-03T12:00:36.350Z",
-          "__v": 0
-        },
-        {
-          "_id": "64a2b859014bc162789af3317",
-          "user": "64972653d9a8bee801567330",
-          "title": "React",
-          "description": "Complete React Course",
-          "tag": "study",
-          "date": "2023-07-03T12:01:20.922Z",
-          "__v": 0
-        },
-        {
-          "_id": "64a2b864414bc162789af3315",
-          "user": "64972653d9a8bee801567330",
-          "title": "Movie",
-          "description": "Complete movie OMG 2",
-          "tag": "entertainment",
-          "date": "2023-07-03T12:00:36.350Z",
-          "__v": 0
-        },
-        {
-          "_id": "64a2b890134bc162789af3317",
-          "user": "64972653d9a8bee801567330",
-          "title": "React",
-          "description": "Complete React Course",
-          "tag": "study",
-          "date": "2023-07-03T12:01:20.922Z",
-          "__v": 0
-        },
-        {
-          "_id": "64a2b826414bc162789af3315",
-          "user": "64972653d9a8bee801567330",
-          "title": "Movie",
-          "description": "Complete movie OMG 2",
-          "tag": "entertainment",
-          "date": "2023-07-03T12:00:36.350Z",
-          "__v": 0
-        },
-        {
-          "_id": "64a22b89014bc162789af3317",
-          "user": "64972653d9a8bee801567330",
-          "title": "React",
-          "description": "Complete React Course",
-          "tag": "study",
-          "date": "2023-07-03T12:01:20.922Z",
-          "__v": 0
-        }
-      ];
+const NoteState = (props) => {
+  const notesInitial = [
+    {
+      "_id": "64a2b86414bc162789af30315",
+      "user": "64972653d9a8bee801567330",
+      "title": "Movie",
+      "description": "Complete movie OMG 2",
+      "tag": "entertainment",
+      "date": "2023-07-03T12:00:36.350Z",
+      "__v": 0
+    },
+    {
+      "_id": "64a2b899014bc162789af3317",
+      "user": "64972653d9a8bee801567330",
+      "title": "React",
+      "description": "Complete React Course",
+      "tag": "study",
+      "date": "2023-07-03T12:01:20.922Z",
+      "__v": 0
+    },
+    {
+      "_id": "64a2b86414bc1627889af3315",
+      "user": "64972653d9a8bee801567330",
+      "title": "Movie",
+      "description": "Complete movie OMG 2",
+      "tag": "entertainment",
+      "date": "2023-07-03T12:00:36.350Z",
+      "__v": 0
+    },
+    {
+      "_id": "64a2b89014bc1627789af3317",
+      "user": "64972653d9a8bee801567330",
+      "title": "React",
+      "description": "Complete React Course",
+      "tag": "study",
+      "date": "2023-07-03T12:01:20.922Z",
+      "__v": 0
+    },
+    {
+      "_id": "64a2b866414bc162789af3315",
+      "user": "64972653d9a8bee801567330",
+      "title": "Movie",
+      "description": "Complete movie OMG 2",
+      "tag": "entertainment",
+      "date": "2023-07-03T12:00:36.350Z",
+      "__v": 0
+    },
+    {
+      "_id": "64a2b859014bc162789af3317",
+      "user": "64972653d9a8bee801567330",
+      "title": "React",
+      "description": "Complete React Course",
+      "tag": "study",
+      "date": "2023-07-03T12:01:20.922Z",
+      "__v": 0
+    },
+    {
+      "_id": "64a2b864414bc162789af3315",
+      "user": "64972653d9a8bee801567330",
+      "title": "Movie",
+      "description": "Complete movie OMG 2",
+      "tag": "entertainment",
+      "date": "2023-07-03T12:00:36.350Z",
+      "__v": 0
+    },
+    {
+      "_id": "64a2b890134bc162789af3317",
+      "user": "64972653d9a8bee801567330",
+      "title": "React",
+      "description": "Complete React Course",
+      "tag": "study",
+      "date": "2023-07-03T12:01:20.922Z",
+      "__v": 0
+    },
+    {
+      "_id": "64a2b826414bc162789af3315",
+      "user": "64972653d9a8bee801567330",
+      "title": "Movie",
+      "description": "Complete movie OMG 2",
+      "tag": "entertainment",
+      "date": "2023-07-03T12:00:36.350Z",
+      "__v": 0
+    },
+    {
+      "_id": "64a22b89014bc162789af3317",
+      "user": "64972653d9a8bee801567330",
+      "title": "React",
+      "description": "Complete React Course",
+      "tag": "study",
+      "date": "2023-07-03T12:01:20.922Z",
+      "__v": 0
+    }
+  ];
 
-    const [notes, setNotes] = useState(notesInitial)
+  const [notes, setNotes] = useState(notesInitial)
 
-    return(
-        <NoteContext.Provider value={{notes,setNotes}}>
-            {props.children}
-        </NoteContext.Provider>
-        );
+  return (
+    <NoteContext.Provider value={{ notes, setNotes }}>
+      {props.children}
+    </NoteContext.Provider>
+  );
 }
 
 export default NoteState;
